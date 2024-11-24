ab493fa90d2395ed8422cf1398f7b7c2dc328847
JuanDavidDV <juandadazav@gmail.com>
2024-11-24 14:31:13 -0500

    front end form created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from './components/Login';
 
 function App() {
 
 
   return (
     <>
-      Hello world
+      <Login />
     </>
   )
 }
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState(null);
+
   return (
-    <section>
-      <div>
-        <h1>Login Page</h1>
-        <form>
-          <div>
-            <label htmlFor="email">Please enter your email:</label>
-            <input type="email" name="email" placeholder="name@email.com"/>
-          </div>
-        </form>
+    <section className="d-flex flex-column  vh-100 bg-body-secondary justify-content-center align-items-center">
+      <h1 className="text-center p-2">Welcome</h1>
+      <div className="d-flex flex-row justify-content-center align-items-center">
+        <div className="p-3 rounded border border-primary bg-primary-subtle" style={{width: "25rem", height: "20rem"}}>
+          <h2 className="pb-3">Login Page</h2>
+          <form>
+            <div className="pb-3">
+              <label htmlFor="email"><b>Please enter your email:</b></label>
+              <input className="form-control rounded-0" type="email" id="email" name="email" placeholder="name@email.com"/>
+            </div>
+            <div className="pb-3">
+              <label htmlFor="password"><b>Please enter your password:</b></label>
+              <input className="form-control rounded-0" type="password" id="password" name="password" placeholder="password"/>
+            </div>
+            <button className="btn btn-primary w-100 rounded-0">Login</button>
+          </form>
+        </div>
       </div>
     </section>
   )
