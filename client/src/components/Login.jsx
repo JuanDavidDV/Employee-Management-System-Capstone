4a36150fa9ea924a78508b610249d9315804f618
JuanDavidDV <juandadazav@gmail.com>
2024-11-24 12:54:33 -0500

    login component introduced and bootstrap applied

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,17 @@
+const Login = () => {
+  return (
+    <section>
+      <div>
+        <h1>Login Page</h1>
+        <form>
+          <div>
+            <label htmlFor="email">Please enter your email:</label>
+            <input type="email" name="email" placeholder="name@email.com"/>
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Login;
