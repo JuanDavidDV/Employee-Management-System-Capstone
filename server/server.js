3562c76636c6276f99b3a22b5233395909dd184f
JuanDavidDV <juandadazav@gmail.com>
2024-11-24 22:06:56 -0500

    include admin route, add cors and error handler

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 import express from "express";
 import "dotenv/config";
+import adminRoutes from "./routes/admin.js";
 import cors from "cors";
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.listen(8080, () => {
-    console.log(`Sever is running`)
-})
+app.use(cors());
+app.use(express.json());  //Middleware to parse JSON on body-parser
+
+// Root route confirm successful connection to user
+app.get("/", (req, res) => {
+  res.send("You have successfully connected to the server.")
+});
+
+app.use("/admin", adminRoutes);  //Incorporates admin route
+
+// 404 handler
+ app.use((req, res) => {
+  res.status(404).send("The route you are trying to connect does not exist");
+});
+
+app.listen(PORT, () => {
+  console.log(`Sever is running at http://localhost:${PORT}`);
+});
