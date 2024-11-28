71ed4d1dc132e674ee9578d9f4cccc04bc99d994
JuanDavidDV <juandadazav@gmail.com>
2024-11-27 23:34:45 -0500

    seeds for categories finalized

--- server/seed-data/categories.js
+++ server/seed-data/categories.js
@@ -4,9 +4,15 @@ export default [
     name: "Information Technology"
   },
   {
-
+    id: 2,
+    name: "Human Resources"
   },
   {
-    
+    id: 3,
+    name: "Information Data Management"
+  },
+  {
+    id: 4,
+    name: "Accounting"
   }
 ]
--- server/seeds/categories.js
+++ server/seeds/categories.js
@@ -1,13 +1,7 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
-};
+// import seed data files, arrays of objects
+import categoriesData from "../seed-data/categories.js";
+
+export async function seed(knex) {
+  await knex("categories").del();
+  await knex("categories").insert(categoriesData);
+}
