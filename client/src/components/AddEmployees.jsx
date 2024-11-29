7f755419d6b27563ec55955341691fa55236caa0
JuanDavidDV <juandadazav@gmail.com>
2024-11-28 22:54:47 -0500

    new employee form created

--- client/src/components/AddEmployees.jsx
+++ client/src/components/AddEmployees.jsx
@@ -1,7 +1,96 @@
 
 
 const AddEmployees = () => {
+  return (
+    <section className="d-flex flex-column h-75 justify-content-center align-items-center mt-2 pb-5">
+      <div className="d-flex flex-column flex-lg-row justify-content-center align-items-center w-100 pt-4">
+        <div
+          className="p-3 rounded border border-primary bg-primary-subtle"
+          style={{ maxWidth: "28rem", width: "90%", minHeight: "45em" }}
+        >
+          <h2 className="pb-3">Add a New Employee</h2>
 
+          <form>
+            <div className="pb-3">
+              <div className="pb-3">
+                <label htmlFor="name" className="form-label"><b>Please enter the new employee's full name:</b></label>
+                <input
+                  className="form-control rounded"
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="John Smith"
+                />
+              </div>
+
+              <div className="pb-3">
+                <label htmlFor="inputGroupFile01" className="form-label"><b>Please enter the new employee's profile picture:</b></label>
+                <input
+                  className="form-control rounded"
+                  type="file"
+                  id="inputGroupFile01"
+                />
+              </div>
+
+              <div className="pb-3">
+                <label htmlFor="password" className="form-label"><b>Please create a password for the new employee:</b></label>
+                <input
+                  className="form-control rounded"
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Enter password"
+                />
+              </div>
+
+              <div className="pb-3">
+                <label htmlFor="email" className="form-label"><b>Please enter the new employee's email:</b></label>
+                <input
+                  className="form-control rounded"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="jsmith@email.com"
+                />
+              </div>
+
+              <div className="pb-3">
+                <label htmlFor="category" className="form-label"><b>Please select the new employee's category:</b></label>
+                <select name="category" id="category">
+                
+                </select>
+              </div>
+
+              <div className="pb-3">
+                <label htmlFor="salary" className="form-label"><b>Please enter the new employee's salary:</b></label>
+                <input
+                  className="form-control rounded"
+                  type="text"
+                  id="salary"
+                  name="salary"
+                  placeholder="Enter salary"
+                />
+              </div>
+
+              <div className="pb-3">
+                <label htmlFor="address" className="form-label"><b>Please enter the new employee's address:</b></label>
+                <input
+                  className="form-control rounded"
+                  type="text"
+                  id="address"
+                  name="address"
+                  placeholder="Enter address"
+                />
+              </div>
+
+            </div>
+
+            <button className="btn btn-primary w-100 rounded">Create New Employee</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default AddEmployees;
--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,13 +1,12 @@
 import NavBar from "../components/NavBar/NavBar";
-import Dashboard from "../components/Dashboard";
 import { Outlet } from "react-router-dom";
 
 const AdminPage = () => {
   return (
     <>
       <NavBar />
-      <main className="bg-success bg-opacity-10 vh-100">
-        <div className="col">
+      <main className="bg-success bg-opacity-10 min-vh-100">
+        <div className="col h-100">
           <div className="p-3 d-flex justify-content-center shadow ">
             <h3 className="text-primary">Employee Management System</h3>
           </div>
