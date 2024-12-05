a357b661593e3f359b92b50fed1c9d9d4cf476d1
JuanDavidDV <juandadazav@gmail.com>
2024-12-05 18:43:26 -0500

    make table responsive

--- client/src/components/Employees/Employees.jsx
+++ client/src/components/Employees/Employees.jsx
@@ -26,46 +26,49 @@ const Employees = () => {
   console.log(employees)
 
   return (
-  <section className="px-5 mt-5">
-    <div className="d-flex justify-content-center">
-      <h2>Employees</h2>
-    </div>
-    <div className="mt-4">
+<section className="px-5 mt-5">
+  <div className="d-flex justify-content-center">
+    <h2 className="fs-4 fs-md-2 fs-lg-1">Employees</h2>
+  </div>
+  <div className="mt-4">
+    <div className="table-responsive">
       <table className="table table-striped">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Salary $(CAD)</th>
-            <th>Image</th>
-            <th>Address</th>
-            <th>Action</th>
+            <th className="fs-6 fs-md-5">Name</th>
+            <th className="fs-6 fs-md-5">Email</th>
+            <th className="fs-6 fs-md-5">Salary</th>
+            <th className="fs-6 fs-md-5">Image</th>
+            <th className="fs-6 fs-md-5">Address</th>
+            <th className="fs-6 fs-md-5">Action</th>
           </tr>
         </thead>
         <tbody>
           {
             employees.map((employee) => (
               <tr key={employee.id}>
-                <td className="align-middle">{employee.name}</td>
-                <td className="align-middle">{employee.email}</td>
-                <td className="align-middle">{employee.salary}</td>
-                <td><img src={baseUrl + employee.image} alt={employee.name} className="employee_picture" />
+                <td className="align-middle fs-6 fs-md-5">{employee.name}</td>
+                <td className="align-middle fs-6 fs-md-5">{employee.email}</td>
+                <td className="align-middle fs-6 fs-md-5">{employee.salary}</td>
+                <td>
+                  <img src={baseUrl + employee.image} alt={employee.name} className="employee_picture" />
                 </td>
-                <td className="align-middle">{employee.address}</td>
+                <td className="align-middle fs-6 fs-md-5">{employee.address}</td>
                 <td className="align-middle">
                   <button className="btn btn-danger me-2">Delete</button>
                   <button className="btn btn-warning">Edit</button>
                 </td>
               </tr>
             ))
-
           }
         </tbody>
       </table>
     </div>
+  </div>
 
-    <Link to="/admin/employees/new" className="btn btn-success mt-3"> Add a New Employee</Link>
-  </section>
+  <Link to="/admin/employees/new" className="btn btn-success mt-3"> Add a New Employee</Link>
+</section>
+
   )
 }
 
