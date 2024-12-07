3d52f72820587492f1a52cae0580626db5b7ef57
JuanDavidDV <juandadazav@gmail.com>
2024-12-07 13:12:23 -0500

    create API employee login

--- client/src/pages/EmployeeLoginPage/EmployeeLoginPage.jsx
+++ client/src/pages/EmployeeLoginPage/EmployeeLoginPage.jsx
@@ -1,7 +1,72 @@
-const EmployeeLoginPage = () => {
-  return (
-    <section>
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
+const baseUrl = import.meta.env.VITE_API_URL;
+
+const EmployeeLoginPage = () => {
+  const [account, setAccount] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(baseUrl + "/employee/login", account, {withCredentials: true}); // Ensures token is send in the request
+      if (data) {
+        navigate("/admin/dashboard")
+      } else {
+        setError(data.message || "An unexpected error occurred" )
+      }
+      
+    }
+    catch(error) {
+      console.error(error);
+      setError("Incorrect email or password")
+    }
+  }
+
+  return (
+    <section className="d-flex flex-column vh-100 bg-body-secondary justify-content-center align-items-center homepage">
+      <h1 className="text-center p-2 text-white">Welcome</h1>
+      <div className="d-flex flex-column flex-lg-row justify-content-center align-items-center w-100">
+        <div 
+          className="p-3 rounded border border-primary bg-primary-subtle"
+          style={{ maxWidth: "25rem", width: "90%", height: "20rem" }}>
+          <h2 className="pb-3">Employee Login</h2>
+          {error && <div className="text-danger">{error}</div> }
+          <form onSubmit={loginUser}>
+            <div className="pb-3">
+              <label htmlFor="email"><b>Please enter your email:</b></label>
+              <input 
+                onChange={(e) => setAccount({...account, email : e.target.value})}
+                className="form-control rounded" 
+                type="email" 
+                id="email" 
+                name="email" 
+                placeholder="name@email.com" 
+              />
+            </div>
+            <div className="pb-3">
+              <label htmlFor="password"><b>Please enter your password:</b></label>
+              <input
+                onChange={(e) => setAccount({...account, password : e.target.value})} 
+                className="form-control rounded" 
+                type="password" 
+                id="password" 
+                name="password" 
+                placeholder="password" 
+              />
+            </div>
+            <button className="btn btn-primary w-100 rounded">Login</button>
+          </form>
+        </div>
+      </div>
     </section>
   )
 };
