bedff39a449845b19a25a06830b66231294f5cd5
JuanDavidDV <juandadazav@gmail.com>
2024-12-07 13:05:42 -0500

    create a folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HomePage from "./pages/HomePage/HomePage";
 import AdminLoginPage from './pages/AdminLoginPage/AdminLoginPage';
+import EmployeeLoginPage from "./pages/EmployeeLoginPage/EmployeeLoginPage";
 import AdminPage from "./pages/AdminPage";
 import Dashboard from "./components/Dashboard";
 import Employees from "./components/Employees/Employees";
@@ -21,7 +22,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin/login" element={<AdminLoginPage />} />
-        <Route path="/employee/login" element={<AdminLoginPage />} />
+        <Route path="/employee/login" element={<EmployeeLoginPage />} />
         <Route path="/admin" element={<AdminPage />}> 
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="categories" element={<Categories />} />
