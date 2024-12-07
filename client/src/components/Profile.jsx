f47907681eff77ba358329cddc73e2d891a72c09
JuanDavidDV <juandadazav@gmail.com>
2024-12-07 17:40:29 -0500

    front end admin profile API created

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,4 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const baseUrl = import.meta.env.VITE_API_URL;
+
 const Profile = () => {
+  const [adminDetails, setAdminDetails] = useState([]);
+
+  const fetchAdmin = async () => {
+    try {
+      const { data } = await axios.get(baseUrl + "/admin/profile")
+      setAdminDetails(data);
+    }
+    catch(error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAdmin();
+  }, []);
+
+  return (
+    <section>
+      <div className="d-flex justify-content-center flex-column align-items-center mt-3">
+        <div className="d-flex align-items-center flex-column mt-4">
+          <h4 className="text-center">Name: {employeeDetails.name}</h4>
+          <h4 className="text-center">Salary: ${employeeDetails.salary}</h4>
+          <h4 className="text-center">Email: {employeeDetails.email}</h4>
+        </div>
+        <div className="mt-2">
+          <button className="btn btn-danger me-3" onClick={logoutEmployee}>Logout</button>
+          <button className="btn btn-warning">Edit</button>
+        </div>
+      </div>
+    </section>
+  )
 
 }
 
