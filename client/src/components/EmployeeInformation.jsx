0d7d46a04ce9a2ad9c1833912a73fcacbb7a9fd7
JuanDavidDV <juandadazav@gmail.com>
2024-12-07 15:45:59 -0500

    create employee detail API front end

--- client/src/components/EmployeeInformation.jsx
+++ client/src/components/EmployeeInformation.jsx
@@ -1,6 +1,24 @@
-import { useParams } from "react-router"
+import { useParams } from "react-router";
+import { useState, useEffect } from "react";
 
 const EmployeeInformation = () => {
+  const {id} = useParams();
+
+  const fetchEmployee = async () => {
+    try {
+      const { data } = await axios.get(baseUrl + "/employee/information/" + id);
+      console.log(data)
+      
+    }
+    catch(error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEmployee();
+  }, []);
+
   return (
     <section>
       helloghnm
